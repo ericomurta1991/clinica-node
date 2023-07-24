c40f85e165dd2f79d293f512a0e0334c9f4f586b
ericomurta1991 <erico_murta@yahoo.com.br>
2023-07-24 10:22:30 -0300

    adicionado import da rota planoDeSaudeRoutes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import pacientes from "../routes/pacientesRoutes.js";
+import medicos from "../routes/medicosRoutes.js"
+import planodesaude from "../routes/planoDeSaudeRoutes.js"
 
 const routes = (app) => {
     app.route("/").get((req,res) => {
