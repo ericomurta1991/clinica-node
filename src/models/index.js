60371e0bb4c5f8ec33930dec1143b49d79f2bfb2
ericomurta1991 <erico_murta@yahoo.com.br>
2023-08-14 20:01:13 -0300

    criado index.js dentro de models para concentrar todos os models da api para importacao

---
+++ src/models/index.js
@@ -0,0 +1,8 @@
+import "./validadorGlobal.js";
+import consultas from "./Consulta.js";
+import medicos from "./Medico.js";
+import pacientes from "./Paciente.js";
+import planoDeSaude from "./PlanoDeSaude.js";
+
+
+export {consultas, medicos, pacientes, planoDeSaude};
